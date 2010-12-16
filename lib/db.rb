3ad0c918584a5f167257eaa6a738c528cfe9364f
Tim Felgentreff <timfelgentreff@gmail.com>
2010-12-16 17:34:56 +0100

    commit the classes once they are persisted

--- lib/db.rb
+++ lib/db.rb
@@ -3,6 +3,7 @@ if RUBY_ENGINE == "maglev"
   DB = MaglevDB
   Project.maglev_persistable
   User.maglev_persistable
+  Maglev.commit_transaction
 else
   require File.expand_path("../db/pstore_db", __FILE__)
   DB = PstoreDB
