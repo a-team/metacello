3926ec3dd0bcaf24cb997504a4904f261243cbe2
Tim Felgentreff <timfelgentreff@gmail.com>
2011-01-22 19:10:41 +0100

    use unobtrusive JS and JQueryUI

--- public/javascript/metacello.coffee
+++ public/javascript/metacello.coffee
@@ -1,10 +1,28 @@
-document.toggle_form = (link) ->
-  $(".#{link}_form").children("form").toggle()
+Metacello = {}
+
+Metacello.availableProjects = ->
+  $("#available-projects").data('names')
+
+Metacello.getAvailableProjects = ->
+  $.getJSON("/projects/names", (data) ->
+    $('#available-projects').data('names', data))
 
 $(document).ready ->
+  document.Metacello = Metacello
+  Metacello.getAvailableProjects()
+  $("#account-edit").click ->
+    $(".account_form").children("form").toggle()
+    false
+  $("#login-signup").click ->
+    $(".login_form").children("form").toggle()
+    false
+  $("#search-box").autocomplete({
+      source: Metacello.availableProjects,
+      minLength: 0
+  })
   if $("#info-header").children().size() > 1
     $("#info-header").slideDown()
-  $(".add-system-link").onClick ->
+  $(".add-system-link").click ->
     # %label{:for => "project[compatibility][#{idx}][system]"} Smalltalk:
     # %input{:type => "textbox", :name => "project[compatibility][#{idx}][system]", :value => "#{h(system)}"}
     # %label{:for => "project[compatibility][#{idx}][versions]"} Versions:
--- public/javascript/metacello.js
+++ public/javascript/metacello.js
@@ -1,12 +1,33 @@
 (function() {
-  document.toggle_form = function(link) {
-    return $("." + link + "_form").children("form").toggle();
+  var Metacello;
+  Metacello = {};
+  Metacello.availableProjects = function() {
+    return $("#available-projects").data('names');
+  };
+  Metacello.getAvailableProjects = function() {
+    return $.getJSON("/projects/names", function(data) {
+      return $('#available-projects').data('names', data);
+    });
   };
   $(document).ready(function() {
+    document.Metacello = Metacello;
+    Metacello.getAvailableProjects();
+    $("#account-edit").click(function() {
+      $(".account_form").children("form").toggle();
+      return false;
+    });
+    $("#login-signup").click(function() {
+      $(".login_form").children("form").toggle();
+      return false;
+    });
+    $("#search-box").autocomplete({
+      source: Metacello.availableProjects,
+      minLength: 0
+    });
     if ($("#info-header").children().size() > 1) {
       $("#info-header").slideDown();
     }
-    return $(".add-system-link").onClick(function() {
+    return $(".add-system-link").click(function() {
       return false;
     });
   });
